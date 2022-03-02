3ff0f84065a38129d3f037fed640c36ec14cad75
Steven Wu <steven.wu.udn@gmail.com>
2022-03-02 14:38:30 +0800

    fix: config

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
             // directory in /public during development and build, so
             // assuming your favicons are in /static/favicons,
             // you can reference them here
-            // src: `/pwa-icons/kai.jpeg`,
+            src: `/pwa-icons/kai.jpeg`,
             type: `image/jpeg`
           }
         ]
